c9e91f0b435c5f1915abed616b979a3ed83cba65
Rosidahsidahhh <nururrosidahh@gmail.com>
2024-04-29 12:39:12 +0700

    First Commit

---
+++ practice.py
@@ -0,0 +1,9 @@
+import requests
+
+api_key = '06c6a6e7-f965-45ea-bbf8-261d5ed28a27'
+word = 'voluminous'
+root_url = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json'
+final_url = f'{root_url}/{word}?key={api_key}'
+r = requests.get(final_url)
+result = r.json()
+print(result)
